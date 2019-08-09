980b9a24b59a48a7997602d2026ae77b17b6fb4f
Jeason Centaure <jcentaur@student.42.fr>
2019-08-09 12:45:54 +0200

    chore: deploy to heroku

--- src/server/app.js
+++ src/server/app.js
@@ -5,10 +5,19 @@ import config from 'server/config'
 import { connect as connectDatabase } from 'server/services/database'
 import apolloServer from 'server/graphql/apolloServer'
 import cors from 'cors'
+import react from 'server/middlewares/react'
+import morgan from 'morgan'
+import path from 'path'
 
 const app = express()
+
 app.use(cors({ origin: 'http://localhost:8080', credentials: true }))
+app.use(morgan('dev'))
+app.use(express.static(path.resolve(__dirname, '../../public')))
+
 apolloServer.applyMiddleware({ app })
+
+app.use(react)
 
 app.listen(config.get('server.port'), () => {
   connectDatabase()
